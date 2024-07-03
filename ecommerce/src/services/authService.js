1d86ae2489f526ebdf1c5e22f46d14fdad07591f
Dayane Floriani Antunes <36275146+dfloriani@users.noreply.github.com>
2024-07-03 11:55:43 -0300

    better error handling for debug

--- ecommerce/src/services/authService.js
+++ ecommerce/src/services/authService.js
@@ -1,22 +1,32 @@
-export async function login(authDetail){
+export async function login(authDetail) {
     const requestOptions = {
         method: "POST",
-        headers: {"content-Type": "application/json"},
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(authDetail)
-    }
-    const response = await fetch(`${process.env.REACT_APP_HOST}/login`, requestOptions);
-    if(!response.ok){
-        let message = { message: response.statusText, status: response.status }
-        throw message;
-    }
-    const data = await response.json();
+    };
 
-    if(data.accessToken){
-    sessionStorage.setItem("token", JSON.stringify(data.accessToken));
-    sessionStorage.setItem("cbid", JSON.stringify(data.user.id));
-    }
+    try {
+        const response = await fetch(`${process.env.REACT_APP_HOST}/login`, requestOptions);
+        
+        if (!response.ok) {
+            const errorDetails = await response.text();
+            let message = { message: response.statusText, status: response.status, details: errorDetails };
+            console.error('Login failed:', message);
+            throw message;
+        }
 
-    return data;
+        const data = await response.json();
+
+        if (data.accessToken) {
+            sessionStorage.setItem("token", JSON.stringify(data.accessToken));
+            sessionStorage.setItem("cbid", JSON.stringify(data.user.id));
+        }
+
+        return data;
+    } catch (error) {
+        console.error('An error occurred during login:', error);
+        throw error;
+    }
 }
 
 export async function register(authDetail){
